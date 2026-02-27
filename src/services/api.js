9722648fd5a82b558d13a91dcb4adf83e5cd0051
Derrick <dectech900@gmail.com>
2026-02-27 01:28:59 -0600

    baseurl changed to production

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1';
+const API_URL = import.meta.env.VITE_API_URL || 'https://farmers.api.dectechgh.com/api/v1';
+// const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1';
 
 const api = axios.create({
   baseURL: API_URL,
